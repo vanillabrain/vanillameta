47cbda500e769483ab864fb1a02b91ad0dc26ed3
ivy <iu8133@gmail.com>
2022-09-20 20:51:16 +0900

    [feat] ace editor 추가

--- frontend-web/src/pages/Widget/WidgetCreate/WidgetAttributeSelect.tsx
+++ frontend-web/src/pages/Widget/WidgetCreate/WidgetAttributeSelect.tsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import { Grid } from '@mui/material';
-import chartData from '@/data/sample/chart.json';
 import componentList from '@/data/componentList.json';
 import LineChartSetting from '@/widget/settings/LineChartSetting';
 import LineChart from '@/modules/linechart/LineChart';
@@ -8,12 +7,14 @@ import PieChart from '@/modules/piechart/PieChart';
 import PieChartSetting from '@/widget/settings/PieChartSetting';
 import WidgetBox from '@/components/widget/WidgetBox';
 import { WIDGET_TYPE } from '@/constant';
+import { get } from '@/helpers/apiHelper';
 
 function WidgetAttributeSelect(props) {
   const { dataSetId, componentType, prevOption, setWidgetOption, setIsValid, isSubmit } = props;
 
   const defaultComponentData = componentList.find(item => item.id === componentType && item);
   const [option, setOption] = useState(defaultComponentData.option);
+  const [data, setData] = useState(null);
 
   const defaultChart = {
     chart: null,
@@ -24,52 +25,65 @@ function WidgetAttributeSelect(props) {
   const [switchChart, setSwitchChart] = useState(defaultChart);
 
   useEffect(() => {
+    getData();
+  }, []);
+
+  const getData = () => {
+    // dataSetId 로 데이터 조회
+    get('/data/sample/chart.json').then(response => {
+      setData(response.data);
+    });
+  };
+
+  useEffect(() => {
     console.log('option changed', option);
 
-    const ChartProps = {
-      option: option,
-      dataSet: chartData,
-    };
+    if (option && data) {
+      const ChartProps = {
+        option: option,
+        dataSet: data,
+      };
 
-    const ChartSettingProps = {
-      option: option,
-      setOption: setOption,
-      setIsValid: setIsValid,
-      isSubmit: isSubmit,
-    };
+      const ChartSettingProps = {
+        option: option,
+        setOption: setOption,
+        setIsValid: setIsValid,
+        isSubmit: isSubmit,
+      };
 
-    switch (componentType) {
-      case WIDGET_TYPE.CHART_LINE:
-        setSwitchChart({
-          ...switchChart,
-          chart: <LineChart {...ChartProps} componentType="line" />,
-          chartSetting: <LineChartSetting {...ChartSettingProps} />,
-        });
-        break;
-      case WIDGET_TYPE.CHART_BAR:
-        setSwitchChart({
-          ...switchChart,
-          chart: <LineChart {...ChartProps} componentType="bar" />,
-          chartSetting: <LineChartSetting {...ChartSettingProps} />,
-        });
-        break;
-      case WIDGET_TYPE.CHART_PIE:
-        setSwitchChart({
-          ...switchChart,
-          chart: <PieChart {...ChartProps} />,
-          chartSetting: <PieChartSetting {...ChartSettingProps} />,
-        });
-        break;
+      switch (componentType) {
+        case WIDGET_TYPE.CHART_LINE:
+          setSwitchChart({
+            ...switchChart,
+            chart: <LineChart {...ChartProps} componentType="line" />,
+            chartSetting: <LineChartSetting {...ChartSettingProps} />,
+          });
+          break;
+        case WIDGET_TYPE.CHART_BAR:
+          setSwitchChart({
+            ...switchChart,
+            chart: <LineChart {...ChartProps} componentType="bar" />,
+            chartSetting: <LineChartSetting {...ChartSettingProps} />,
+          });
+          break;
+        case WIDGET_TYPE.CHART_PIE:
+          setSwitchChart({
+            ...switchChart,
+            chart: <PieChart {...ChartProps} />,
+            chartSetting: <PieChartSetting {...ChartSettingProps} />,
+          });
+          break;
 
-      default:
-        setSwitchChart({
-          ...switchChart,
-          chart: <LineChart {...ChartProps} componentType="line" />,
-          chartSetting: <LineChartSetting {...ChartSettingProps} />,
-        });
-        break;
+        default:
+          setSwitchChart({
+            ...switchChart,
+            chart: <LineChart {...ChartProps} componentType="line" />,
+            chartSetting: <LineChartSetting {...ChartSettingProps} />,
+          });
+          break;
+      }
     }
-  }, [option, componentType, chartData, isSubmit]);
+  }, [option, componentType, data]);
 
   // 이미 저장된 위젯값이 있는 경우 불러오기
   useEffect(() => {
