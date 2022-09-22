4fff74f26345ae6cca2191555724db77042b64c9
HeeseonYoon <yathoo@gmail.com>
2022-09-22 15:29:46 +0900

    backend setup_ing

---
+++ backend-api/src/app.module.ts
@@ -0,0 +1,33 @@
+import {Module} from '@nestjs/common';
+import {AppController} from './app.controller';
+import {AppService} from './app.service';
+import {TestModule} from './test/test.module';
+import {ConfigModule} from '@nestjs/config';
+import {TypeOrmModule} from '@nestjs/typeorm';
+
+@Module({
+    imports: [
+        ConfigModule.forRoot({
+            isGlobal: true,
+            envFilePath: process.env.NODE_ENV == 'dev' ? '.env.dev' : '.env.prod',
+        }),
+        TypeOrmModule.forRoot({
+            type: 'mysql',
+            host: process.env.DB_HOST,
+            port: parseInt(process.env.DB_PORT) || 3306,
+            username: process.env.DB_USERNAME,
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_NAME,
+            autoLoadEntities: true,
+            entities: [__dirname + '/**/*.entity{.ts,.js}'],
+            synchronize: false,
+            logging: process.env.NODE_ENV == 'dev',
+            retryAttempts: 1,
+        })
+        , TestModule
+    ],
+    controllers: [AppController],
+    providers: [AppService],
+})
+export class AppModule {
+}
