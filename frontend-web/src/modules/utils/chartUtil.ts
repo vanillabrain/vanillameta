d0bff50fede0eaecbadd289bbd41038f2f4c1be3
ivy <iu8133@gmail.com>
2022-09-23 16:44:42 +0900

    [feat] add chart legend position property

---
+++ frontend-web/src/modules/utils/chartUtil.ts
@@ -0,0 +1,60 @@
+/**
+ *
+ * @param position
+ */
+export const getLegendOtion = position => {
+  let option = {};
+  switch (position) {
+    case 'left':
+      option = {
+        orient: 'vertical',
+        left: 10,
+        top: 'center',
+      };
+      break;
+    case 'right':
+      option = {
+        orient: 'vertical',
+        right: 10,
+        top: 'center',
+      };
+      break;
+    case 'top':
+      option = {
+        orient: 'horizontal',
+        top: 'top',
+      };
+      break;
+    case 'bottom':
+      option = {
+        orient: 'horizontal',
+        top: 'bottom',
+      };
+      break;
+  }
+  return option;
+};
+
+/**
+ *
+ * @param position
+ */
+
+export const getGridSize = position => {
+  let option = {};
+  switch (position) {
+    case 'left':
+      option = { top: 50, right: 10, bottom: 50, left: 100 };
+      break;
+    case 'right':
+      option = { top: 50, right: 100, bottom: 50, left: 10 };
+      break;
+    case 'top':
+      option = { top: 80, right: 50, bottom: 20, left: 50 };
+      break;
+    case 'bottom':
+      option = { top: 20, right: 50, bottom: 80, left: 50 };
+      break;
+  }
+  return option;
+};
