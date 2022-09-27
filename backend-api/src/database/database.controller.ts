4e84db19284d25835bc696096b6d4a73f6ce28e3
Dosun Yun <dosunyun@vanillabrain.com>
2022-09-27 21:41:51 +0900

    [backend-api] Add connection.service.ts

--- backend-api/src/database/database.controller.ts
+++ backend-api/src/database/database.controller.ts
@@ -3,10 +3,14 @@ import { DatabaseService } from './database.service';
 import { CreateDatabaseDto } from './dto/create-database.dto';
 import { UpdateDatabaseDto } from './dto/update-database.dto';
 import { QueryExecuteDto } from './dto/query-execute.dto';
+import { ConnectionService } from './connection/connection.service';
 
 @Controller('database')
 export class DatabaseController {
-  constructor(private readonly databaseService: DatabaseService) {}
+  constructor(
+    private readonly databaseService: DatabaseService,
+    private readonly connectionService: ConnectionService,
+  ) {}
 
   @Post()
   create(@Body() createDatabaseDto: CreateDatabaseDto) {
@@ -15,12 +19,12 @@ export class DatabaseController {
 
   @Post('test')
   testConnection(@Body() createDatabaseDto: CreateDatabaseDto) {
-    return this.databaseService.testConnection(createDatabaseDto);
+    return this.connectionService.testConnection(createDatabaseDto);
   }
 
   @Post('execute')
   executeQuery(@Body() queryExecuteDto: QueryExecuteDto) {
-    return this.databaseService.executeQuery(queryExecuteDto);
+    return this.connectionService.executeQuery(queryExecuteDto);
   }
 
   @Get()
--- backend-api/src/database/database.module.ts
+++ backend-api/src/database/database.module.ts
@@ -3,10 +3,11 @@ import { DatabaseService } from './database.service';
 import { DatabaseController } from './database.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Database } from './entities/database.entity';
+import { ConnectionService } from './connection/connection.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Database])],
   controllers: [DatabaseController],
-  providers: [DatabaseService],
+  providers: [DatabaseService, ConnectionService],
 })
 export class DatabaseModule {}
