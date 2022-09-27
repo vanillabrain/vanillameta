eea83996540c3146f684cf0837e75993faed9bb7
HeeseonYoon <yathoo@gmail.com>
2022-09-27 18:08:32 +0900

    [feat] template response type 설정

--- backend-api/src/template/template.controller.ts
+++ backend-api/src/template/template.controller.ts
@@ -7,80 +7,81 @@ import {UpdateTemplateItemDto} from "./dto/update-template-item.dto";
 
 @Controller('template')
 export class TemplateController {
-  constructor(private readonly templateService: TemplateService) {}
+    constructor(private readonly templateService: TemplateService) {
+    }
 
-  /**
-   * 템플릿 생성
-   * @param createTemplateDto
-   */
-  @Post()
-  create(@Body() createTemplateDto: CreateTemplateDto) {
-    return this.templateService.create(createTemplateDto);
-  }
+    /**
+     * 템플릿 생성
+     * @param createTemplateDto
+     */
+    @Post()
+    create(@Body() createTemplateDto: CreateTemplateDto) {
+        return this.templateService.create(createTemplateDto);
+    }
 
-  /**
-   * 템플릿 상세 아이템 생성
-   * @param createTemplateItemDto
-   */
-  @Post('/item')
-  createItem(@Body() createTemplateItemDto: CreateTemplateItemDto) {
-    return this.templateService.createItem(createTemplateItemDto);
-  }
+    /**
+     * 템플릿 상세 아이템 생성
+     * @param createTemplateItemDto
+     */
+    @Post('/item')
+    createItem(@Body() createTemplateItemDto: CreateTemplateItemDto) {
+        return this.templateService.createItem(createTemplateItemDto);
+    }
 
-  /**
-   * 템플릿 목록 조회
-   */
-  @Get()
-  findAll() {
-    return this.templateService.findAll();;
-  }
+    /**
+     * 템플릿 목록 조회
+     */
+    @Get()
+    findAll() {
+        return this.templateService.findAll();
+    }
 
-  /**
-   * 템플릿 단건 조회
-   * @param id
-   */
-  @Get(':id')
-  findOne(@Param('id') id: number) {
-    return this.templateService.findOne(id);
-  }
+    /**
+     * 템플릿 단건 조회
+     * @param id
+     */
+    @Get(':id')
+    findOne(@Param('id') id: number) {
+        return this.templateService.findOne(id);
+    }
 
-  /**
-   * 템플릿 단건 업데이트
-   * @param id
-   * @param updateTemplateDto
-   */
-  @Put(':id')
-  update(@Param('id') id: string, @Body() updateTemplateDto: UpdateTemplateDto) {
-    return this.templateService.update(+id, updateTemplateDto);
-  }
+    /**
+     * 템플릿 단건 업데이트
+     * @param id
+     * @param updateTemplateDto
+     */
+    @Put(':id')
+    update(@Param('id') id: string, @Body() updateTemplateDto: UpdateTemplateDto) {
+        return this.templateService.update(+id, updateTemplateDto);
+    }
 
-  /**
-   * 템플릿 상세 아이템 단건 업데이트
-   * @param id
-   * @param updateTemplateDto
-   */
-  @Put('/item/:id')
-  updateItem(@Param('id') id: string, @Body() updateTemplateItemDto: UpdateTemplateItemDto) {
-    return this.templateService.updateItem(+id, updateTemplateItemDto);
-  }
+    /**
+     * 템플릿 상세 아이템 단건 업데이트
+     * @param id
+     * @param updateTemplateDto
+     */
+    @Put('/item/:id')
+    updateItem(@Param('id') id: string, @Body() updateTemplateItemDto: UpdateTemplateItemDto) {
+        return this.templateService.updateItem(+id, updateTemplateItemDto);
+    }
 
-  /**
-   * 템플릿 삭제 (사용여부 N 처리)
-   * @param id
-   */
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.templateService.remove(+id);
-  }
+    /**
+     * 템플릿 삭제 (사용여부 N 처리)
+     * @param id
+     */
+    @Delete(':id')
+    remove(@Param('id') id: string) {
+        return this.templateService.remove(+id);
+    }
 
-  /**
-   * 템플릿 추천 목록 조회
-   * @param widgets
-   */
-  //todo:: yhs:: 추천 알고리즘 적용해서 조회해 와야함
-  @Post('/recommend')
-  findRecommendAll(@Body() widgets: any[]) {
-    this.templateService.findRecommendTemplates(widgets);
-    return null;
-  }
+    /**
+     * 템플릿 추천 목록 조회
+     * @param widgets
+     */
+    //todo:: yhs:: 추천 알고리즘 적용해서 조회해 와야함
+    @Post('/recommend')
+    findRecommendAll(@Body() widgets: any[]) {
+        this.templateService.findRecommendTemplates(widgets);
+        return null;
+    }
 }
