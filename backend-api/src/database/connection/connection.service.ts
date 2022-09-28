2e1f04c87fabc2a8cb3ed8bec7ab66d133ea796b
Dosun Yun <dosunyun@vanillabrain.com>
2022-09-28 15:01:53 +0900

    [backend-api] Add connection.service.ts business logic

--- backend-api/src/database/connection/connection.service.ts
+++ backend-api/src/database/connection/connection.service.ts
@@ -1,32 +1,79 @@
 import { Injectable } from '@nestjs/common';
 import { CreateDatabaseDto } from '../dto/create-database.dto';
 import { QueryExecuteDto } from '../dto/query-execute.dto';
-import { KnexOptions } from '@nestjsplus/knex';
+import { DatabaseService } from '../database.service';
+import { Knex, knex } from 'knex';
 
-const Knex = require('knex');
-const knexConnections = new Map<string, any>();
+const knexConnections = new Map<number, Knex>();
 
 @Injectable()
 export class ConnectionService {
-  addKnex(name: string, options: KnexOptions) {
-    if (!this.hasKnex(name)) {
-      knexConnections.set(name, new Knex(options));
+  constructor(private readonly databaseService: DatabaseService) {}
+
+  /**
+   * Knex 객체 생성 후 pool에 추가
+   * @param id
+   * @param options
+   */
+  addKnex(id: number, options: Knex.Config) {
+    if (!this.hasKnex(id)) {
+      knexConnections.set(id, knex(options));
     }
   }
 
-  removeKnex(name: string) {
-    knexConnections.delete(name);
+  /**
+   * Knex 객체 pool에서 삭제
+   * @param id
+   */
+  removeKnex(id: number) {
+    knexConnections.delete(id);
   }
 
-  hasKnex(name: string): boolean {
-    return knexConnections.has(name);
+  /**
+   * Knex 객체 pool 안에 존재 유무
+   * @param id
+   */
+  hasKnex(id: number): boolean {
+    return knexConnections.has(id);
   }
 
-  getKnex(name: string) {
-    return knexConnections.get(name);
+  /**
+   * Knex 객체 가져오기 - 만약 없으면 가져오기
+   * @param id
+   */
+  async getKnex(id: number): Promise<Knex> {
+    if (!this.hasKnex(id)) {
+      const one = await this.databaseService.findOne(id);
+      this.addKnex(id, one.knexConfig as Knex.Config);
+    }
+    return knexConnections.get(id);
   }
 
-  async testConnection(createDatabaseDto: CreateDatabaseDto) {}
+  /**
+   * 데이터베이스 연결 테스트
+   * @param createDatabaseDto
+   */
+  async testConnection(createDatabaseDto: CreateDatabaseDto): Promise<boolean> {
+    let _knex = knex(createDatabaseDto.knexConfig as Knex.Config);
+    try {
+      await _knex.raw('SELECT 1');
+      console.log('knex connected');
+      return true;
+    } catch (e) {
+      console.log('knex not connected');
+      console.error(e);
+      return false;
+    } finally {
+      await _knex.destroy();
+    }
+  }
 
-  async executeQuery(queryExecuteDto: QueryExecuteDto) {}
+  /**
+   * 쿼리 실행
+   * @param queryExecuteDto
+   */
+  async executeQuery(queryExecuteDto: QueryExecuteDto) {
+    const knex = await this.getKnex(queryExecuteDto.id);
+    return knex.raw(queryExecuteDto.query);
+  }
 }
