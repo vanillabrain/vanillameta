703d13fc2f8cae863381b86fe58390f772f6afe7
heeseonyoon <heeseon.yoon@vanillabrain.com>
2022-10-15 02:21:35 +0900

    [BE-database] Add get data api (/database/data)

--- backend-api/src/database/database.controller.ts
+++ backend-api/src/database/database.controller.ts
@@ -1,9 +1,11 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
 import { DatabaseService } from './database.service';
 import { CreateDatabaseDto } from './dto/create-database.dto';
 import { UpdateDatabaseDto } from './dto/update-database.dto';
 import { QueryExecuteDto } from './dto/query-execute.dto';
 import { ConnectionService } from '../connection/connection.service';
+import { DatasetType } from '../common/enum/dataset-type.enum';
+import { ResponseStatus } from '../common/enum/response-status.enum';
 
 @Controller('database')
 export class DatabaseController {
@@ -12,16 +14,37 @@ export class DatabaseController {
     private readonly connectionService: ConnectionService,
   ) {}
 
+  @Get('/data')
+  async findData(
+    @Query('datasetType') datasetType: DatasetType,
+    @Query('datasetId') datasetId: number,
+  ) {
+    const res = await this.databaseService.findData(datasetType, datasetId);
+    return { status: ResponseStatus.SUCCESS, data: res };
+  }
+
+  /**
+   * 데이터베이스 생성 ( 데이터소스 생성)
+   * @param createDatabaseDto
+   */
   @Post()
   create(@Body() createDatabaseDto: CreateDatabaseDto) {
     return this.databaseService.create(createDatabaseDto);
   }
 
+  /**
+   * 데이터베이스 연결 테스트
+   * @param createDatabaseDto
+   */
   @Post('test')
   testConnection(@Body() createDatabaseDto: CreateDatabaseDto) {
     return this.connectionService.testConnection(createDatabaseDto);
   }
 
+  /**
+   * 쿼리 실행
+   * @param queryExecuteDto
+   */
   @Post('execute')
   executeQuery(@Body() queryExecuteDto: QueryExecuteDto) {
     return this.connectionService.executeQuery(queryExecuteDto);
@@ -32,8 +55,8 @@ export class DatabaseController {
    */
   @Get()
   async findAll() {
-    const resultList = await this.databaseService.findAll();
-    return resultList;
+    const res = await this.databaseService.findAll();
+    return res;
   }
 
   /**
--- backend-api/src/database/database.module.ts
+++ backend-api/src/database/database.module.ts
@@ -5,9 +5,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Database } from './entities/database.entity';
 import { ConnectionService } from '../connection/connection.service';
 import { Dataset } from '../dataset/entities/dataset.entity';
+import { TableQuery } from '../widget/tabel-query/entity/table-query.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Database, Dataset])],
+  imports: [TypeOrmModule.forFeature([Database, Dataset, TableQuery])],
   controllers: [DatabaseController],
   providers: [DatabaseService, ConnectionService],
 })
