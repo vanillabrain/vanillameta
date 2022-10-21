01588b8206d2280848330b7ff1f90ed80852c174
SA K <sakang07@gmail.com>
2022-10-21 09:53:20 +0900

    [FE-chart] Fix error of waterfall chart step 3 #83

--- frontend-web/src/widget/settings/WaterfallChartSetting.tsx
+++ frontend-web/src/widget/settings/WaterfallChartSetting.tsx
@@ -1,23 +1,17 @@
 import React from 'react';
-import { Divider, ListItem, ListItemText } from '@mui/material';
+import { Grid, List, ListItem, ListItemText, styled } from '@mui/material';
 import SelectForm from '@/components/form/SelectForm';
-import { handleChange } from '@/widget/utils/handler';
-import { AGGREGATION_LIST, COLUMN_TYPE, LEGEND_LIST } from '@/constant';
-import ColorButtonForm from '@/components/form/ColorButtonForm';
-import ColorPickerForm from '@/components/form/ColorPickerForm';
+import { handleChange, handleSeriesChange } from '@/widget/utils/handler';
+import { AGGREGATION_LIST, COLUMN_TYPE, LEGEND_LIST, WIDGET_AGGREGATION } from '@/constant';
+import ColorFieldForm from '@/components/form/ColorFieldForm';
 
 const WaterfallChartSetting = props => {
   const { option, setOption, axis = 'x', spec } = props;
 
-  const handleSeriesChange = (event, prop) => {
-    console.log(event.target.name, event.target.value);
-    const name = event.target.name.slice(0, -1);
-
-    setOption(prevState => {
-      const obj = { ...prevState };
-      obj.series[prop][name] = event.target.value;
-      return obj;
-    });
+  // 컴포넌트 별 default series
+  const defaultSeries = {
+    field: '',
+    aggregation: WIDGET_AGGREGATION.SUM,
   };
 
   return (
@@ -40,75 +34,40 @@ const WaterfallChartSetting = props => {
         <ListItemText primary="시리즈 설정" />
         <SelectForm
           required={true}
-          id="field1"
-          name="field1"
-          label="수입"
+          id="field"
+          name="field"
+          label="필드"
           labelField="columnName"
           valueField="columnType"
           optionList={spec.filter(item => item.columnType === COLUMN_TYPE.NUMBER).map(item => item.columnName)}
-          value={option.series.income.field}
-          onChange={event => handleSeriesChange(event, 'income')}
-          endButton={
-            <ColorPickerForm
-              name={`color1`}
-              color={option.series.income.color}
-              onChange={event => handleSeriesChange(event, 'income')}
-            />
-          }
+          value={option.series[0].field}
+          onChange={event => handleSeriesChange(event, setOption)}
         />
         <SelectForm
-          id="aggregation1"
-          name="aggregation1"
+          id="aggregation"
+          name="aggregation"
           label="집계 방식"
           optionList={AGGREGATION_LIST}
-          value={option.series.income.aggregation}
-          onChange={event => handleSeriesChange(event, 'income')}
-          disabledDefaultValue
-        />
-        <Divider />
-        <SelectForm
-          required={true}
-          id="field2"
-          name="field2"
-          label="지출"
-          labelField="columnName"
-          valueField="columnType"
-          optionList={spec.filter(item => item.columnType === COLUMN_TYPE.NUMBER).map(item => item.columnName)}
-          value={option.series.expenses.field}
-          onChange={event => handleSeriesChange(event, 'expenses')}
-          endButton={
-            <ColorPickerForm
-              name={`color2`}
-              color={option.series.expenses.color}
-              onChange={event => handleSeriesChange(event, 'expenses')}
-            />
-          }
-        />
-        <SelectForm
-          id="aggregation2"
-          name="aggregation2"
-          label="집계 방식"
-          optionList={AGGREGATION_LIST}
-          value={option.series.expenses.aggregation}
-          onChange={event => handleSeriesChange(event, 'expenses')}
+          value={option.series[0].aggregation}
+          onChange={event => handleSeriesChange(event, setOption)}
           disabledDefaultValue
         />
       </ListItem>
-      {/*<ListItem divider>*/}
-      {/*  <ListItemText primary="색상 설정" />*/}
-      {/*  {option.color.map((item, index) => (*/}
-      {/*    <React.Fragment key={index}>*/}
-      {/*      <ColorFieldForm*/}
-      {/*        id={`color${index + 1}`}*/}
-      {/*        name={`color${index + 1}`}*/}
-      {/*        value={option.color[index]}*/}
-      {/*        colorList={option.color}*/}
-      {/*        setOption={setOption}*/}
-      {/*        index={index}*/}
-      {/*      />*/}
-      {/*    </React.Fragment>*/}
-      {/*  ))}*/}
-      {/*</ListItem>*/}
+      <ListItem divider>
+        <ListItemText primary="색상 설정" />
+        {option.color.map((item, index) => (
+          <React.Fragment key={index}>
+            <ColorFieldForm
+              id={`color${index + 1}`}
+              name={`color${index + 1}`}
+              value={option.color[index]}
+              colorList={option.color}
+              setOption={setOption}
+              index={index}
+            />
+          </React.Fragment>
+        ))}
+      </ListItem>
       <ListItem>
         <ListItemText>범례 설정</ListItemText>
         <SelectForm
