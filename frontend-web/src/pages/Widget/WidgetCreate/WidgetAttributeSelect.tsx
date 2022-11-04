719c9723e37576ab182c0c2ecbf56d0c9f957bbb
SA K <sakang07@gmail.com>
2022-11-04 17:56:48 +0900

    [FE-widget] Modify chart layout

--- frontend-web/src/pages/Widget/WidgetCreate/WidgetAttributeSelect.tsx
+++ frontend-web/src/pages/Widget/WidgetCreate/WidgetAttributeSelect.tsx
@@ -96,12 +96,14 @@ const WidgetAttributeSelect = props => {
       <Box
         sx={{
           width: '60%',
-          height: '500px',
+          minHeight: '500px',
+          maxHeight: '100%',
           margin: '54px auto',
           border: '1px solid #e2e2e2',
           borderRadius: '8px',
           boxShadow: '2px 2px 9px 0 rgba(42, 50, 62, 0.1), 0 4px 4px 0 rgba(0, 0, 0, 0.02)',
           backgroundColor: '#fff',
+          overflow: 'hidden',
         }}
       >
         <WidgetViewer
