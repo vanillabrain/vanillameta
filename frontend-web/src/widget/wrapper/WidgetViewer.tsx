ce165d3ae77d3c6e230cc2512445000825b60b1f
SA K <sakang07@gmail.com>
2022-11-07 19:05:16 +0900

    [FE-data] Fix a bug that tooltip is not visible when mixed donut chart and score board mouse over #240

--- frontend-web/src/widget/wrapper/WidgetViewer.tsx
+++ frontend-web/src/widget/wrapper/WidgetViewer.tsx
@@ -365,6 +365,8 @@ const WidgetViewer = props => {
                 top: '50%',
                 left: 0,
                 right: 0,
+                maxWidth: 'fit-content',
+                height: 0,
                 margin: 'auto',
                 transform: 'translateY(-50%)',
               }}
@@ -384,6 +386,8 @@ const WidgetViewer = props => {
                 top: '50%',
                 left: 0,
                 right: 0,
+                maxWidth: 'fit-content',
+                height: 0,
                 margin: 'auto',
                 transform: 'translateY(-50%)',
               }}
