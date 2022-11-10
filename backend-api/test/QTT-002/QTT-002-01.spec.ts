8799782a27bb152ec18fbe34ed38b686e126ecf0
손승우 <seungwoo@vanillabrain.com>
2022-11-10 19:33:06 +0900

    test: update QTT-002, QTT-003 test script

--- backend-api/test/QTT-002/QTT-002-01.spec.ts
+++ backend-api/test/QTT-002/QTT-002-01.spec.ts
@@ -5,14 +5,18 @@ import { Database } from "../../src/database/entities/database.entity";
 import { ConfigModule } from '@nestjs/config';
 import { TableQuery } from "../../src/widget/tabel-query/entity/table-query.entity";
 import { WidgetService } from "../../src/widget/widget.service";
-import * as widgetTestoption from "../../widgetTestoption-10.json"
+import * as widgetTestoption from "../../widgetTestOption-10.json";
 import { Widget } from "../../src/widget/entities/widget.entity";
 import { Component } from "../../src/component/entities/component.entity";
 import { TableQueryService } from "../../src/widget/tabel-query/table-query.service";
+import {Connection, DataSource, getConnection, getRepository } from 'typeorm';
 
 
 describe('Check Widget Function', () => {
     let widgetService: WidgetService;
+    let widgetRepository: Widget;
+    let connection: Connection;
+
     beforeEach(async () => {
         const module: TestingModule = await Test.createTestingModule({
             imports: [
@@ -23,9 +27,11 @@ describe('Check Widget Function', () => {
                 getTestMysqlModule(),
                 TypeOrmModule.forFeature([Widget, Component, TableQuery, Database]),
             ],
-            providers: [WidgetService, TableQueryService],
+            providers: [WidgetService, TableQueryService, Widget],
         }).compile();
         widgetService = module.get<WidgetService>(WidgetService);
+        widgetRepository = module.get<Widget>(Widget);
+        connection = module.get<Connection>(Connection);
 
     }, 10000);
 
@@ -35,12 +41,24 @@ describe('Check Widget Function', () => {
             "message": "success"
         }
     }
-    for(let i = 0; i < widgetTestoption['default'].length; i ++){
-        it('QTT-002-01: widget 생성 확인', async() => {
-            const result = await widgetService.create(Object.create(widgetTestoption['default'][i]));
-            return expect(result).toBeDefined();
-        });
-    }
+
+    it('QTT-002-01: widget 생성 확인', async() => {
+
+        // const result = await widgetService.create(Object.create(widgetTestoption['default']))
+        // widgetTestoption['default'].option = JSON.stringify(widgetTestoption['default'].option)
+        widgetTestoption['default'].forEach(el => {
+            el.option = JSON.stringify(el.option)
+        })
+        console.log(widgetTestoption['default'])
+        const user = await connection
+            .createQueryBuilder()
+            .insert()
+            .into(Widget)
+            .values(widgetTestoption['default'])
+            .execute()
+        return expect(user).toBeDefined();
+    });
+
     it('QTT-002-02: widget 목록 확인', async() => {
         const result = await widgetService.findAll();
         return expect(result).toEqual(result)
