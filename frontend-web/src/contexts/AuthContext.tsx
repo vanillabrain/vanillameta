9f9a635ab4096ccfb4da47987161a5b494e26e46
SA K <sakang07@gmail.com>
2022-11-25 14:48:28 +0900

    [FE-auth] Connect sign up and login authorization API: step 2 #298

--- frontend-web/src/contexts/AuthContext.tsx
+++ frontend-web/src/contexts/AuthContext.tsx
@@ -1,30 +1,18 @@
-import { createContext, useContext, useState } from 'react';
+import { createContext, useState } from 'react';
 import authService from '@/api/authService';
-import axios from 'axios';
+import { removeToken, setToken } from '@/helpers/authHelper';
+import { useNavigate } from 'react-router-dom';
 
 export const AuthContext = createContext(null);
 
 export const AuthProvider = ({ children }) => {
-  const [accessToken, setAccessToken] = useState(null);
-  const [isLogin, setIsLogin] = useState(false);
-  const [userInfo, setUserInfo] = useState(null);
-
-  const handleCheckLogin = async () => {
-    return await axios.get('/data/dummyUser2.json').then(response => {
-      if (response.status === 200) {
-        console.log(response);
-        setIsLogin(true);
-        setUserInfo(response.data.data);
-        console.log(isLogin, userInfo);
-      }
-    });
-    // return authService.getUser().then(response => {
-    //   if (response.status === 200) {
-    //     setIsLogin(true);
-    //     setUserInfo(response.data);
-    //   }
-    // });
-  };
+  // const [accessToken, setAccessToken] = useState(null);
+  const navigate = useNavigate();
+  const [userState, setUserState] = useState({
+    isLogin: false,
+    userId: null,
+    email: null,
+  });
 
   const handleLogin = async (id, pwd) => {
     const data = {
@@ -36,26 +24,27 @@ export const AuthProvider = ({ children }) => {
       .then(response => {
         console.log(response, 'response');
         if (response.status === 201) {
-          setIsLogin(true);
-          setAccessToken(response.data.accessToken);
+          console.log(response.data.accessToken);
+          setToken(response.data.accessToken);
+          navigate('/dashboard');
+          // setAccessToken(response.data.accessToken);
         }
       })
       .catch(error => {
-        setIsLogin(false);
+        removeToken();
+        // setAccessToken(null);
         console.log(error, error.response.data.data.message);
         throw error;
       });
   };
 
   const handleLogout = () => {
-    setIsLogin(false);
+    removeToken();
   };
 
   const value = {
-    isLogin,
-    userInfo,
-    token: accessToken,
-    // checkLogin: handleCheckLogin,
+    userState,
+    setUserState,
     onLogin: handleLogin,
     onLogout: handleLogout,
   };
--- frontend-web/src/router/ProtectedRoute.tsx
+++ frontend-web/src/router/ProtectedRoute.tsx
@@ -1,19 +1,44 @@
-import { Navigate } from 'react-router-dom';
+import React, { useContext, useEffect } from 'react';
+import { Navigate, useNavigate } from 'react-router-dom';
 import { AuthContext } from '@/contexts/AuthContext';
-import { useContext, useEffect } from 'react';
-import { LoadingContext } from '@/contexts/LoadingContext';
+import { getToken } from '@/helpers/authHelper';
+import authService from '@/api/authService';
 
 export const ProtectedRoute = ({ children }) => {
-  const { token, isLogin } = useContext(AuthContext);
-  const { showLoading, hideLoading } = useContext(LoadingContext);
+  const navigate = useNavigate();
+  const { userState, setUserState } = useContext(AuthContext);
+
+  // accessToken을 localStorage에서 꺼내기
+  const token = getToken();
+  console.log('token:', token);
 
   useEffect(() => {
-    showLoading();
-    // checkLogin();
-    console.log('ProtectedRoute', isLogin);
-  }, [isLogin]);
+    getUserState();
+  }, [token]);
 
-  if (!isLogin && !token) {
+  const getUserState = () => {
+    if (token) {
+      // accessToken이 localStorage에 있으면 유저정보 받아오기
+      authService
+        .getUser()
+        .then(response => {
+          if (response.status === 201) {
+            setUserState({ isLogin: true, userId: response.data.userId, email: response.data.email });
+            console.log(userState, '유저정보');
+          }
+        })
+        .catch(error => {
+          navigate('/login');
+          throw error;
+        });
+    } else {
+      // accessToken이 localStorage에 없으면 RefreshToken으로 accessToken 재발급받기
+      // 다시 처음으로
+      // getUserState();
+    }
+  };
+
+  if (!token) {
     return <Navigate to="/login" replace />;
   }
 
