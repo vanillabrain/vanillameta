4c267dbd91ad58d2bd6a8c4f900280788c8f0f99
SA K <sakang07@gmail.com>
2022-12-09 20:25:21 +0900

    [FE] Fix DatePicker

--- frontend-web/src/components/form/DatePicker.tsx
+++ frontend-web/src/components/form/DatePicker.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import TextField from '@mui/material/TextField';
+import 'dayjs/locale/ko';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { DatePicker as MuiDatePicker } from '@mui/x-date-pickers/DatePicker';
@@ -7,11 +8,7 @@ import { DatePicker as MuiDatePicker } from '@mui/x-date-pickers/DatePicker';
 const DatePicker = ({ shareLimitDate, setShareLimitDate }) => {
   console.log(shareLimitDate);
   return (
-    <LocalizationProvider
-      adapterLocale={'ko'}
-      dateAdapter={AdapterDayjs}
-      localeText={{ start: 'Check-in', end: 'Check-out' }}
-    >
+    <LocalizationProvider adapterLocale="ko" dateAdapter={AdapterDayjs}>
       <MuiDatePicker
         value={shareLimitDate}
         onChange={newValue => {
@@ -30,9 +27,9 @@ const DatePicker = ({ shareLimitDate, setShareLimitDate }) => {
               sx={{
                 width: '130px',
                 color: '#4a4a4a',
-                '& .MuiOutlinedInput-root': { pr: '8px' },
-                '& .MuiInputAdornment': { p: 0 },
-                input: { pl: '10px' },
+                '& .MuiOutlinedInput-root': { pl: '10px', pr: '5px' },
+                '& .MuiInputAdornment-root': { p: 0 },
+                input: { px: 0 },
                 '& .MuiSvgIcon-root': { color: '#4a4a4a' },
               }}
             />
