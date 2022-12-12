ba0634e0da9a52af5880609451a5e76c029a5671
손승우 <seungwoo@vanillabrain.com>
2022-12-12 19:39:28 +0900

    add: add swagger

--- backend-api/src/app.module.ts
+++ backend-api/src/app.module.ts
@@ -28,7 +28,7 @@ import { ShareUrlModule } from './share-url/share-url.module';
       port: parseInt(process.env.DB_PORT) || 3306,
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
-      database: 'vanillameta_auth',
+      database: process.env.DB_NAME,
       autoLoadEntities: true,
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: false,
