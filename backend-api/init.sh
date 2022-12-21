1b623d5257040fa4816b794e3aa2dfdf7290c2d8
손승우 <seungwoo@vanillabrain.com>
2022-12-21 19:16:19 +0900

    add: add docker-compose

--- backend-api/init.sh
+++ backend-api/init.sh
@@ -1,4 +1,9 @@
-#!/usr/bin/env bash
+#!/bin/bash
+    #init.sh
+#    mysql_config_editor set --login-path=test --host=dev-sellerking-service.cpxfjdgipx1h.ap-northeast-2.rds.amazonaws.com --user=vanillabrain --password
+#    Enter password: qkslffk123123
 
-docker build -t mm .
-docker-compose up -d
+    mysqldump --single-transaction --databases vanillameta_auth -h dev-sellerking-service.cpxfjdgipx1h.ap-northeast-2.rds.amazonaws.com -u vanillabrain -pqkslffk123123 < /docker-entrypoint-initdb.d/dump_data/dump_data.sql
+    #    mysqldump -h dev-sellerking-service.cpxfjdgipx1h.ap-northeast-2.rds.amazonaws.com -u vanillabrain --password > dump_data.sql
+
+    #end of init.sh
