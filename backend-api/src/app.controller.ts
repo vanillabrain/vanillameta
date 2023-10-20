f1c5279b1c9640684274f00cb4b37d02028d5339
Dosun Yun <dosunyun@vanillabrain.com>
2023-10-20 22:47:02 +0900

    [BE] Add check server ip

--- backend-api/src/app.controller.ts
+++ backend-api/src/app.controller.ts
@@ -9,4 +9,9 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('/ip')
+  async getIp(): Promise<string> {
+    return await this.appService.getIp();
+  }
 }
