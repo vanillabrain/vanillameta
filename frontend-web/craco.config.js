a5a70554cea052a9564cefb6a83147738051e306
윤도선 <dosunyun@vanillabrain.com>
2024-10-01 19:55:09 +0900

    feat: webpack 빌드 청크 업데이트

--- frontend-web/craco.config.js
+++ frontend-web/craco.config.js
@@ -10,4 +10,39 @@ module.exports = {
       },
     },
   ],
+  webpack: {
+    configure: (webpackConfig, { env, paths }) => {
+      // 청크 분할 설정
+      webpackConfig.optimization.splitChunks = {
+        chunks: 'all',
+        maxInitialRequests: Infinity,
+        minSize: 20000,
+        cacheGroups: {
+          vendor: {
+            test: /[\\/]node_modules[\\/]/,
+            name(module, chunks, cacheGroupKey) {
+              const moduleFileName = module
+                .identifier()
+                .split('/')
+                .reduceRight(item => item);
+              const allChunksNames = chunks.map(item => item.name).join('~');
+              return `${cacheGroupKey}-${allChunksNames}-${moduleFileName}`;
+            },
+          },
+          common: {
+            name: 'common',
+            minChunks: 2,
+            priority: 10,
+            reuseExistingChunk: true,
+            enforce: true,
+          },
+        },
+      };
+
+      // 런타임 청크 설정
+      webpackConfig.optimization.runtimeChunk = 'single';
+
+      return webpackConfig;
+    },
+  },
 };
